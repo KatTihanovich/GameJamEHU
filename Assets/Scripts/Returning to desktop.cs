516823b0a0c74aedd57fc2ca1c18e376a4fbbb9d
alenatsibets <alena.tsibets@gmail.com>
2025-03-16 09:26:01 +0200

    antivirus

--- Assets/Scripts/Returning to desktop.cs
+++ Assets/Scripts/Returning to desktop.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 public class ExitButton : MonoBehaviour
 {
     public string targetScene = "Desktop"; // Название сцены, на которую переходим
+    public string runnerScene = "Runner";
+    public string scaryMazeScene = "ScaryMaze";
+    public string antiVirusWindowScene = "AntyvirusWindow";
+    public string taskManagerScene = "TaskManager";
     public Button exitButton;
 
     void Start()
@@ -18,5 +22,24 @@ public class ExitButton : MonoBehaviour
     public void GoToDesktop()
     {
         SceneManager.LoadScene(targetScene);
+    }
+
+    public void GoToRunner()
+    {
+        SceneManager.LoadScene(runnerScene);
+    }
+
+    public void GoToScaryMaze()
+    {
+        SceneManager.LoadScene(scaryMazeScene);
+    }
+    public void GoToAntyvirusWindow()
+    {
+        SceneManager.LoadScene(antiVirusWindowScene);
+    }
+
+    public void GoToTaskManager()
+    {
+        SceneManager.LoadScene(taskManagerScene);
     }
 }
