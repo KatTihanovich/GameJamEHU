42d6ac99b04eace56b01a2f0998c7541033b45ae
alenatsibets <alena.tsibets@gmail.com>
2025-03-16 07:25:27 +0200

    runner textures

--- Assets/Scripts/Runner/SpikeGenerator.cs
+++ Assets/Scripts/Runner/SpikeGenerator.cs
@@ -2,25 +2,33 @@ using UnityEngine;
 
 public class SpikeGenerator : MonoBehaviour
 {
-    public GameObject spike;
+    public GameObject spikeType1; // First spike type
+    public GameObject spikeType2; // Second spike type
+
     public float MinSpeed;
     public float currentSpeed;
     public float MaxSpeed;
     public float speedMultiplier;
+
     void Awake()
     {
         currentSpeed = MinSpeed;
         GenerateSpike();
     }
+
     public void GenerateNextSpikeWithGap()
     {
-        float randomWait= Random.Range(0.1f, 1.2f);
+        float randomWait = Random.Range(0.1f, 1.2f);
         Invoke("GenerateSpike", randomWait);
     }
+
     void GenerateSpike()
     {
-        GameObject SpikeIns = Instantiate(spike, transform.position, transform.rotation);
-        
+        // Randomly choose between the two spike types
+        GameObject selectedSpike = Random.Range(0, 2) == 0 ? spikeType1 : spikeType2;
+
+        GameObject SpikeIns = Instantiate(selectedSpike, transform.position, transform.rotation);
+
         SpikeIns.GetComponent<SpikeScript>().spikeGenerator = this;
     }
 
@@ -28,7 +36,7 @@ public class SpikeGenerator : MonoBehaviour
     {
         if (currentSpeed < MaxSpeed)
         {
-            currentSpeed += speedMultiplier;
+            currentSpeed += speedMultiplier * Time.deltaTime;
         }
     }
 }
